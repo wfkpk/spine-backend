b0af43efa31b4c3663b7432b861803881c8e9c8a
PK <89706786+wfkpk@users.noreply.github.com>
2023-06-25 03:35:08 +0530

    farzi commit

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
