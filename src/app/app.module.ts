ba2cf8932600bec7e1de86333daa227829ef4725
PK <89706786+wfkpk@users.noreply.github.com>
2023-08-01 01:18:04 +0530

    removed rate limiting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { SearchModule } from 'src/search/search.module';
 import { UserModule } from 'src/user/user.module';
 import { UploadModule } from 'src/upload/upload.module';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+
 @Module({
   imports: [
-    ThrottlerModule.forRoot({
-      ttl: 60,
-      limit: 5,
-    }),
+    //for rate limiting
+    // ThrottlerModule.forRoot({
+    //   ttl: 60,
+    //   limit: 5,
+    // }),
     UserModule,
     PrismaModule,
     BooksModule,
