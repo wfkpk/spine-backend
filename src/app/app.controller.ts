15f25cc6cb3bba64f5c021d5ef8276493d9f3a9d
PK <89706786+wfkpk@users.noreply.github.com>
2023-08-01 01:17:35 +0530

    changes in app controller

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Response } from 'src/interface/response';
 
 @Controller()
 export class AppController {
@@ -15,35 +14,6 @@ export class AppController {
   getPing() {
     return {
       data: 'pong',
-    };
-  }
-
-  @Get('/dashboard/count')
-  async getUserCount(): Promise<Response> {
-    const userCount = this.appService.getUsersCount();
-    return {
-      data: userCount,
-    };
-  }
-
-  @Get('/dashboard/book-count')
-  async getBookCount(): Promise<Response> {
-    return {
-      data: await this.appService.getBooksCount(),
-    };
-  }
-
-  @Get('/dashboard/record-count')
-  async getRecordCount(): Promise<Response> {
-    return {
-      data: await this.appService.getRecordCount(),
-    };
-  }
-
-  @Get('/genre')
-  async getGenre(): Promise<Response> {
-    return {
-      data: await this.appService.getGenre(),
     };
   }
 }
